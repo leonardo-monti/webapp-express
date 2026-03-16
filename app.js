8d38740e2256c2611893cfccc64e205f09b23fbf
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-16 15:58:45 +0100

    add index

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require ("express")
-const app = express("")
+const app = express()
 const port = 3000
+const connection = require("./db/connection")
+
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
 })
+
+//INDEX//
+
+app.get("/movies",(req,res)=>{
+const sql = "SELECT * FROM movies"
+connection.query (sql, (err,results)=>{
+    if(err) {
+            return res.status(500).json({
+                success:false,
+                message: "Database query failed"
+            })
+        }
+        res.json({
+            success:true,
+            message:"Post del database",
+            result:results
+        })
+  })
+})
+
+
+
 
 app.listen(port,()=>{
     console.log("Server listening on http://localhost:3000")
