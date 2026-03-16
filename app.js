61ddaeaa8745aa9cb82ee6e98de3e278e45a50b2
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-16 16:22:01 +0100

    add show

--- app.js
+++ app.js
@@ -22,10 +22,51 @@ connection.query (sql, (err,results)=>{
         }
         res.json({
             success:true,
-            message:"Post del database",
-            result:results
+            message:"Movie of the database",
+            movies:results
         })
   })
+})
+
+//SHOW//
+
+app.get("/movies/:id",(req,res)=>{
+    const movieId=req.params.id
+    const sql="SELECT * FROM movies WHERE id = ?"
+    connection.query (sql,[movieId],(err,results)=>{
+        if(err){
+            console.error(err)
+            return res.status(500).json({
+                success:false,
+                message:"Error while searching movie"
+            })
+        }
+        if(results.length===0){
+            return res.status(404).json({
+                success:false,
+                message:"Movie not found"
+            })
+        }
+       const movie = results[0]
+
+       const sqlReviews= "SELECT * FROM reviews WHERE movie_id = ?"
+       connection.query(sqlReviews,[movieId],(err,reviews)=>{
+        if(err){
+            console.error(err)
+            return res.status(500).json({
+                success:false,
+                message:"Error searching for reviews"
+            })
+        }
+         movie.reviews = reviews
+
+         res.json({
+            success:true,
+            message:"Found movie with reviews",
+            movie:movie
+         })
+       })
+    })
 })
 
 
